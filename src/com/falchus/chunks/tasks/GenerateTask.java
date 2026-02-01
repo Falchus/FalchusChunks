a191182423d748bf138b8580f9e9d852002468d4
Elias <elias@falchus.com>
2026-02-01 18:29:14 +0100

    safety over speed

--- src/com/falchus/chunks/tasks/GenerateTask.java
+++ src/com/falchus/chunks/tasks/GenerateTask.java
@@ -6,7 +6,6 @@ import java.util.Queue;
 import org.bukkit.Bukkit;
 import org.bukkit.GameMode;
 import org.bukkit.Location;
-import org.bukkit.World;
 import org.bukkit.entity.Player;
 
 import com.falchus.chunks.Main;
@@ -20,7 +19,6 @@ public class GenerateTask implements Runnable {
 	private int taskId;
 	
 	private final Player player;
-	private final World world;
 	private final boolean unloadable;
 	
 	private final Queue<ChunkCoord> queue = new ArrayDeque<>();
@@ -32,14 +30,13 @@ public class GenerateTask implements Runnable {
 
     public GenerateTask(Player player, int radius, boolean unloadable) {
         this.player = player;
-        world = player.getWorld();
         this.unloadable = unloadable;
         
         int chunkRadius = radius >> 4;
         int cx = player.getLocation().getBlockX() >> 4;
         int cz = player.getLocation().getBlockZ() >> 4;
-        for (int x = -chunkRadius; x <= chunkRadius; x += 3) {
-            for (int z = -chunkRadius; z <= chunkRadius; z += 3) {
+        for (int x = -chunkRadius; x <= chunkRadius; x++) {
+            for (int z = -chunkRadius; z <= chunkRadius; z++) {
                 queue.add(new ChunkCoord(cx + x, cz + z));
             }
         }
@@ -61,36 +58,28 @@ public class GenerateTask implements Runnable {
 	
 	@Override
 	public void run() {
-		while (!queue.isEmpty()) {		
-	    	ChunkCoord coord = queue.peek();
-			
-	    	if (!world.isChunkLoaded(coord.x, coord.z)) {
-	    		queue.poll();
-	    		
-                if (!unloadable) {
-                    plugin.getChunkManager().getChunks().add(coord);
-                }
-	            
-		        int bx = (coord.x << 4) + 8;
-		        int bz = (coord.z << 4) + 8;
-		        int y = 100;
-		        
-		        Bukkit.dispatchCommand(Bukkit.getConsoleSender(), (Bukkit.getName().equals("FalchusSpigot") ? "falchus" : "") + "spigot:tp " + player.getName() + " " + (bx + 0.5) + " " + y + " " + (bz + 0.5)); // async in FalchusSpigot
-		
-				processed++;
-				
-				if (processed % 10 == 0) {
-		    		int percent = (processed * 100) / total;
-		    		player.sendMessage(Main.prefix + "Generated §a" + processed + "§7/§e" + total + " §7chunks (" + percent + "%).");
-				}
-	    	} else {
-	    		queue.poll();
-	    		processed++;
-	    	}
-		}
-		
 		if (queue.isEmpty()) {
 			finish();
+			return;
+		}
+		
+    	ChunkCoord coord = queue.poll();
+        
+        int bx = (coord.x << 4) + 8;
+        int bz = (coord.z << 4) + 8;
+        int y = 100;
+        
+        Bukkit.dispatchCommand(Bukkit.getConsoleSender(), (Bukkit.getName().equals("FalchusSpigot") ? "falchus" : "") + "spigot:tp " + player.getName() + " " + (bx + 0.5) + " " + y + " " + (bz + 0.5)); // async in FalchusSpigot
+
+        if (!unloadable) {
+            plugin.getChunkManager().getChunks().add(coord);
+        }
+        
+		processed++;
+		
+		if (processed % 10 == 0) {
+    		int percent = (processed * 100) / total;
+    		player.sendMessage(Main.prefix + "Generated §a" + processed + "§7/§e" + total + " §7chunks (" + percent + "%).");
 		}
 	}
 	
